1bb133045b79623964d36aad71a6286288ad3266
Alexis Nava <navajulian652@gmail.com>
2025-02-05 09:37:42 -0600

    Se agrega el array y la funcion para agregar a el amigo

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+/* Agregar amigo al array */
+function agregarAmigo() {
+    const nombreAmigo = document.getElementById("amigo").value.trim();  
+
+    if (!nombreAmigo) {  
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    if (amigos.includes(nombreAmigo)) {  
+        alert("Este nombre ya está en la lista.");
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+    document.getElementById("amigo").value = "";  
+    
+    console.log(amigos);
+}
